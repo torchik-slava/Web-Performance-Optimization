9384ac5ccadf7b556ab5c0786bb6ae041045dc27
Viachaslau Torchyk <viachaslau_torchyk@epam.com>
2023-03-31 19:03:55 +0300

    part 2

--- link-and-script-tag-attributes/Part 2/webpack.config.js
+++ link-and-script-tag-attributes/Part 2/webpack.config.js
@@ -1,30 +1,83 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images',
+module.exports = [
+    {
+        entry: path.resolve(__dirname, 'src/index.js'),
+        output: {
+            filename: 'modern-browsers-bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/i,
+                    loader: "html-loader",
                 },
-            },
-        ]
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                    },
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                      presets: [
+                        ["@babel/preset-env", {
+                            targets: {
+                                esmodules: true
+                            }
+                        }]
+                      ]
+                    }
+                  },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            })
+        ],
+        mode: 'production'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-    mode: 'production'
-};
+    {
+        entry: path.resolve(__dirname, 'src/index.js'),
+        output: {
+            filename: 'old-browsers-bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/i,
+                    loader: "html-loader",
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                    },
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                      presets: [
+                        ["@babel/preset-env"]
+                      ]
+                    }
+                  },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            })
+        ],
+        mode: 'production'
+    }
+];
